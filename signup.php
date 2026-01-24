e05c993675025c58ab834385b57a6205c7bff239
Rida-Fatima-Ali <rida.fatima.naushad.ali>
2026-01-24 16:17:39 +0530

    signup.php added

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php 
+include "db.php";
+
+$username=$_POST['username'];
+$email=$_POST['email'];
+$password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+$sql="INSERT INTO users (username,email,password) VALUES ('$username','$email','$password')";
+
+if(mysqli_query($conn,$sql)){
+    header("location: ../index.html");
+}
+?>
